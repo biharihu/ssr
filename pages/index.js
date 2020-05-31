64212faa1c47e8b4e64ae985e0cd243fe6d3b032
biharihu <bablu630@gmail.com>
2020-06-01 02:20:07 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./index.scss";
+import Card from "./Card";
+
+class Index extends React.Component {
+  // static async getInitialProps() {
+
+  // }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <a href="/ohyeah">
+            <img src="./static/logo.png" className="static-logo" alt="logo" />
+          </a>
+        </header>
+
+        <div className="Grid">
+          {/* {this.props.cards.map((card) => (
+            <Card key={card.id} />
+          ))} */}
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Index;
